3ad4e0c3aef499ece9281c6b1337a3399f78ce36
Sondre Hauk Heier Holte <sondreholte@Sondre-sin-MacBook-Pro.local>
2024-02-26 12:22:27 +0100

    lights.h file updated

---
+++ skeleton_project/source/driver/lights.h
@@ -0,0 +1,16 @@
+#pragma once
+
+//Skulle gjerne hatt en algoritme som returnerer en matrise med alle lysene
+//må nok bruke funskjonen elevio_button_lamp()
+//Kan man bruke samme matrise som i structen QUEUE?
+
+struct elevator_lights
+{
+    int lights[4][3];
+} lights;
+
+
+int update_inside_lights();
+int update_outside_lights();
+
+elevator_lights update_all_lights();
